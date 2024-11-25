77c8b65aa76a9858f0cf16976a7eabaa4beb8cdf
Zhomart Abumansur <abuka7778888@gmail.com>
2024-11-25 11:42:06 +0600

    first commit

---
+++ js1.js
@@ -0,0 +1,20 @@
+let num1 = +prompt("Бірінші санды енгіз")
+let num2 = +prompt("Екінші санды енгіз")
+let tanba = prompt("Tanba jazynyz")
+let score = 0
+
+function Queshion() {
+    if (a + b) {
+        alert("Kosganda " + score + " shygady")
+    } else if (a - b) {
+        alert("Azaitkanda " + score + " shygady")
+    } else if (a * b) {
+        alert("Kobeitkende " + score + " shygady")
+    } else if (a / b) {
+        alert("Bolgende " + score + " shygady")
+    } else {
+        alert("Qate engizu")
+    }
+
+    score++
+}                                                                   
---
+++ js2.js
@@ -0,0 +1,13 @@
+let tauarquny = +prompt("tauardyn qunyn jazynyz")
+let tauarsaby = +prompt("Kansha tauar alasyz")
+
+if (tauarquny >= 5000) {
+    alert(" Тауар 5000мың нан асты және енді сізге 10% жеңілдік")
+} else if (tauarquny >= 10000) {
+    alert(" Тауар 10000мың нан асты және енді сізге 20% жеңілдік")
+} else if (tauarquny >= 20000) {
+    alert(" Тауар 20000мың нан асты және енді сізге 30% жеңілдік")
+} else {
+    alert("Дұрыс жаз!")
+}
+
